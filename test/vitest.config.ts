b011627ed62f8fb193885bc581fe68a1a9dae55d
winches <329487092@qq.com>
2025-02-16 17:29:39 +0800

    feat: add check test cases

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -2,6 +2,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
-  // @ts-expect-error vite-tsconfig-paths is not compatible with vitest plugin type
   plugins: [tsconfigPaths()]
 });
