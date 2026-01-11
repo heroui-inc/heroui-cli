253c04f8bee538652afc60fe056bb32c6105f98a
WK Wong <wingkwong.code@gmail.com>
2026-01-11 16:13:29 +0800

    refactor(helpers): logger

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -5,8 +5,14 @@ import type {SAFE_ANY} from './type';
 import chalk from 'chalk';
 import {default as _gradientString} from 'gradient-string';
 
+/**
+ * Default gradient colors used for CLI branding
+ */
 export const defaultColors = ['#F54180', '#338EF7'] as const;
 
+/**
+ * Pre-configured gradient string function with default colors
+ */
 export const gradientString = _gradientString(...defaultColors);
 
 const logPrefix = gradientString('HeroUI CLI:');
@@ -15,30 +21,35 @@ export type PrefixLogType = Extract<
   keyof typeof Logger,
   'error' | 'gradient' | 'info' | 'log' | 'warn' | 'success'
 >;
-export class Logger {
-  constructor() {}
 
-  static log(...args: Parameters<typeof console.log>) {
+type LogArgs = Parameters<typeof console.log>;
+
+/**
+ * Centralized logging utility for the HeroUI CLI.
+ * Provides colored console output methods for different log levels.
+ */
+export class Logger {
+  static log(...args: LogArgs) {
     console.log(...args);
   }
 
-  static info(...args: Parameters<typeof console.info>) {
+  static info(...args: LogArgs) {
     console.info(...args.map((item) => chalk.blue(item)));
   }
 
-  static success(...args: Parameters<typeof console.info>) {
+  static success(...args: LogArgs) {
     console.info(...args.map((item) => chalk.green(item)));
   }
 
-  static warn(...args: Parameters<typeof console.warn>) {
+  static warn(...args: LogArgs) {
     console.warn(...args.map((item) => chalk.yellow(item)));
   }
 
-  static error(...args: Parameters<typeof console.error>) {
+  static error(...args: LogArgs) {
     console.error(...args.map((item) => chalk.red(item)));
   }
 
-  static grey(...args: Parameters<typeof console.log>) {
+  static grey(...args: LogArgs) {
     console.log(...args.map((item) => chalk.gray(item)));
   }
 
@@ -50,9 +61,9 @@ export class Logger {
     return this[type](logPrefix, ...args);
   }
 
-  static newLine(lines?: number) {
-    if (!lines) lines = 1;
-
-    for (let i = 0; i < lines; i++) this.log();
+  static newLine(lines = 1) {
+    for (let i = 0; i < lines; i++) {
+      this.log();
+    }
   }
 }
