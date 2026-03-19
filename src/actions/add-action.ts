f2fa453530a7bda741ee59476c4886303d9cda78
WK Wong <wingkwong.code@gmail.com>
2026-03-19 12:32:34 +0800

    refactor: add confirmation before action

--- src/actions/add-action.ts
+++ src/actions/add-action.ts
@@ -5,14 +5,16 @@ import chalk from 'chalk';
 import {detect} from '@helpers/detect';
 import {exec} from '@helpers/exec';
 import {Logger} from '@helpers/logger';
-import {getPackageInfo} from '@helpers/package';
+import {outputComponents} from '@helpers/output-info';
+import {getPackageInfo, transformPackageDetail} from '@helpers/package';
 import {resolver} from 'src/constants/path';
 import {HEROUI_PACKAGES} from 'src/constants/required';
+import {getSelect} from 'src/prompts';
 
 export async function addAction(options: CommandOptions) {
   const {packagePath = resolver('package.json')} = options;
 
-  const {allDependenciesKeys} = getPackageInfo(packagePath);
+  const {allDependencies, allDependenciesKeys} = getPackageInfo(packagePath);
 
   const missing = HEROUI_PACKAGES.filter((pkg) => !allDependenciesKeys.has(pkg));
 
@@ -21,10 +23,24 @@ export async function addAction(options: CommandOptions) {
     process.exit(0);
   }
 
+  const components = await transformPackageDetail(missing, allDependencies);
+
+  outputComponents({
+    components,
+    message: chalk.cyanBright('📦 Packages to be installed:')
+  });
+
+  const isConfirmed = await getSelect('Proceed with installation?', [
+    {title: 'Yes', value: true},
+    {title: 'No', value: false}
+  ]);
+
+  if (!isConfirmed) {
+    process.exit(0);
+  }
+
   const currentPkgManager = await detect();
   const runCmd = currentPkgManager === 'npm' ? 'install' : 'add';
-
-  Logger.info(`Adding ${missing.map((c) => chalk.underline(c)).join(', ')}`);
 
   await exec(`${currentPkgManager} ${runCmd} ${missing.join(' ')}`);
 
--- src/actions/remove-action.ts
+++ src/actions/remove-action.ts
@@ -4,14 +4,16 @@ import chalk from 'chalk';
 
 import {detect} from '@helpers/detect';
 import {Logger} from '@helpers/logger';
-import {getPackageInfo} from '@helpers/package';
+import {outputComponents} from '@helpers/output-info';
+import {getPackageInfo, transformPackageDetail} from '@helpers/package';
 import {removeDependencies} from '@helpers/remove';
 import {resolver} from 'src/constants/path';
 import {HEROUI_PACKAGES} from 'src/constants/required';
+import {getSelect} from 'src/prompts';
 
 export async function removeAction(options: CommandOptions) {
   const {packagePath = resolver('package.json')} = options;
-  const {allDependenciesKeys} = getPackageInfo(packagePath);
+  const {allDependencies, allDependenciesKeys} = getPackageInfo(packagePath);
 
   const installed = HEROUI_PACKAGES.filter((pkg) => allDependenciesKeys.has(pkg));
 
@@ -20,9 +22,23 @@ export async function removeAction(options: CommandOptions) {
     process.exit(0);
   }
 
-  const packageManager = await detect();
+  const components = await transformPackageDetail(installed, allDependencies);
 
-  Logger.info(`Removing ${installed.map((c) => chalk.underline(c)).join(', ')}`);
+  outputComponents({
+    components,
+    message: chalk.yellowBright('❗️ Packages slated for removal:')
+  });
+
+  const isConfirmed = await getSelect('Confirm removal of these packages:', [
+    {title: 'Yes', value: true},
+    {title: 'No', value: false}
+  ]);
+
+  if (!isConfirmed) {
+    process.exit(0);
+  }
+
+  const packageManager = await detect();
 
   await removeDependencies([...installed], packageManager);
 
